54e091aa7935cc30549a1ce19e6a5444b5124878
kobanogit <mlme@disroot.org>
2018-09-15 17:18:29 +0200

    added navigation and 2nd page

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,8 +1,12 @@
 const API_TOKEN = "d57e61c3cf4fc715607416aaecd7483f"
 
-export function getFilmsFromApiWithSearchedText (text) {
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+export function getFilmsFromApiWithSearchedText (text, page) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
   return fetch(url)
     .then((response) => response.json())
     .catch((error) => console.error(error))
+}
+
+export function getImageFromApi(name) {
+  return 'https://image.tmdb.org/t/p/w185' + name
 }
--- Components/Search.js
+++ Components/Search.js
@@ -1,5 +1,7 @@
+// Components/Search.js
+
 import React from 'react'
-import {StyleSheet, View, TextInput, Button, FlatList, Text} from 'react-native'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
@@ -7,35 +9,79 @@ class Search extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = {
-        films: []
-    }
     this.searchedText = ""
+    this.page = 0
+    this.totalPages = 0
+    this.state = {
+      films: [],
+      isLoading: false
+    }
+  }
+
+  _displayDetailForFilm = (idFilm) => {
+    this.props.navigation.navigate("FilmDetail", { idFilm: idFilm});
   }
 
   _loadFilms() {
-      if (this.searchedText.length > 0) {
-        getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
-            this.setState({ films: data.results })
-        })
-      }
+    if (this.searchedText.length > 0) {
+      this.setState({ isLoading: true })
+      getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
+          this.page = data.page
+          this.totalPages = data.total_pages
+          this.setState({
+            films: [ ...this.state.films, ...data.results ],
+            isLoading: false
+          })
+      })
+    }
   }
 
   _searchTextInputChanged(text) {
     this.searchedText = text
   }
 
-  render(){
-    console.log("RENDER");
-    return(
+  _searchFilms() {
+    this.page = 0
+    this.totalPages = 0
+    this.setState({
+      films: [],
+    }, () => {
+        this._loadFilms()
+    })
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+        </View>
+      )
+    }
+  }
+
+  render() {
+    return (
       <View style={styles.main_container}>
-        <TextInput onChangeText={(text) => this._searchTextInputChanged(text)} style={styles.textInput} placeholder="Titre du film"/>
-        <Button style={{height: 50}} title="Rechercher" onPress={() => this._loadFilms()}/>
+        <TextInput
+          style={styles.textinput}
+          placeholder='Titre du film'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+          onSubmitEditing={() => this._searchFilms()}
+        />
+        <Button style={{ height: 50 }} title='Rechercher' onPress={() => this._searchFilms()}/>
         <FlatList
           data={this.state.films}
           keyExtractor={(item) => item.id.toString()}
-          renderItem={({item}) => <FilmItem film={item}/>}
+          renderItem={({item}) => <FilmItem film={item} displayDetailForFilm={this._displayDetailForFilm}/>}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+              if (this.state.films.length > 0 && this.page < this.totalPages) { // On vérifie également qu'on n'a pas atteint la fin de la pagination (totalPages) avant de charger plus d'éléments
+                 this._loadFilms()
+              }
+          }}
         />
+        {this._displayLoading()}
       </View>
     )
   }
@@ -43,16 +89,24 @@ class Search extends React.Component {
 
 const styles = StyleSheet.create({
   main_container: {
-    marginTop: 20,
     flex: 1
   },
-  textInput: {
+  textinput: {
     marginLeft: 5,
     marginRight: 5,
     height: 50,
     borderColor: '#000000',
     borderWidth: 1,
     paddingLeft: 5
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
 
